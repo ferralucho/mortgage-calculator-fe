5837c80e011774f931711fcc4c282fcfab2eed9f
Luciano Ferrari <lucianodarioferrari@gmail.com>
2022-11-14 20:04:36 -0300

    app structure and config

---
+++ mortgage-calculator-fe/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>
+          Mortage calculator for Canada</h1>
+        <p>
+        Purchasing a home? Get a sense for your mortgage payments, the cash you'll need to close and the monthly carrying costs
+        </p>
+   
+      <body>
+
+      </body>
+      </header>
+    </div>
+  );
+}
+
+export default App;
