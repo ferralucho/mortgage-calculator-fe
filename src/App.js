88116f88de77e1321520bac220a81de97c125e9a
Luciano Ferrari <lucianodarioferrari@gmail.com>
2022-11-14 22:19:29 -0300

    validation errors

--- src/App.js
+++ src/App.js
@@ -17,14 +17,15 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
       <h2>
           Mortgage amortization calculator for Canada</h2>
         <span>
         Purchasing a home? Get a sense for your mortgage amortization payments, the cash you'll need to close and the monthly carrying costs.
         </span>
         <div>The minimum down payment in Canada is 5%. For down payments of less than 20%, home buyers are required to purchase mortgamortization default insurance (CMHC insurance).</div>
-   
+      </header>
+
       <body>
         <form onSubmit={handleSubmit(onSubmit)}>
           <label>Property Price</label>
@@ -43,18 +44,33 @@ function App() {
               <p>Numbers only</p>
             )}
             <label>Down Payment</label>
-            <input {...register("downPayment", { pattern: /[0-9]+/i })} />
+            <input {...register("downPayment", { required: true,
+              maxLength: 20, pattern: /[0-9]+/i })} />
+            {errors?.downPayment?.type === "required" && <p>This field is required</p>}
+            {errors?.downPayment?.type === "maxLength" && (
+              <p>Down Payment cannot exceed 20 characters</p>
+            )}
             {errors?.downPayment?.type === "pattern" && (
               <p>Numbers only</p>
             )}
             <label>Amortization Period</label>
-            <input {...register("amortization", { min: 5, max: 30 })} />
+            <input {...register("amortization", { required: true,
+              maxLength: 20, min: 5, max: 30 })} />
+            {errors?.amortizationPeriod?.type === "required" && <p>This field is required</p>}
+            {errors?.amortizationPeriod?.type === "maxLength" && (
+              <p>Amortization Period cannot exceed 20 characters</p>
+            )}
             {errors.amortization && (
               <p>Amortization period must be between 5 and 30 years</p>
             )}
 
             <label>Mortgage Rate</label>
-            <input {...register("mortgageRate", { pattern: /[0-9]+/i })} />
+            <input {...register("mortgageRate", {  required: true,
+              maxLength: 20, pattern: /[0-9]+/i })} />
+            {errors?.mortgageRate?.type === "required" && <p>This field is required</p>}
+            {errors?.mortgageRate?.type === "maxLength" && (
+              <p>Down Payment cannot exceed 20 characters</p>
+            )}
             {errors?.mortgageRate?.type === "pattern" && (
               <p>Numbers only</p>
             )}
@@ -62,7 +78,6 @@ function App() {
             <input type="submit" />
         </form>
       </body>
-      </header>
     </div>
   );
 }
